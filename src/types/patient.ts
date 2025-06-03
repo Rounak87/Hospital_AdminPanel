4bbf8c57073e1ceafc28709882b8ff43fd66a903
Rounak singh <rounaksingh0807@gmail.com>
2025-06-03 11:22:00 +0530

    initial setup and redux toolkit setup

---
+++ src/types/patient.ts
@@ -0,0 +1,18 @@
+export interface Address {
+  address: string;
+  city: string;
+  postalCode: string;
+}
+
+export interface PatientAdmission {
+  generatedId: number; 
+  firstName: string;
+  lastName: string;
+  age: number;
+  gender: 'male' | 'female' | 'other';
+  email: string;
+  phone: string;
+  address: Address;
+  status: 'Admitted' | 'Discharged' | 'Under Observation';
+  roomNumber?: string; 
+}
